1b1cdd4856bac4e50336115b654e53f7b7288ccf
Wanyu <q37375@gmail.com>
2021-06-16 15:45:16 +0800

    VO

---
+++ src/main/java/forum/controller/EmpServlet.java
@@ -0,0 +1,104 @@
+package forum.controller;
+
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import com.emp.model.*;
+
+public class EmpServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		doPost(req, res);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+
+		if ("getAll".equals(action)) {
+			/***************************�}�l�d�߸�� ****************************************/
+			EmpDAO dao = new EmpDAO();
+			List<EmpVO> list = dao.getAll();
+
+			/***************************�d�ߧ���,�ǳ����(Send the Success view)*************/
+			HttpSession session = req.getSession();
+			session.setAttribute("list", list);    // ��Ʈw���X��list����,�s�Jsession
+			// Send the Success view
+			String url = "/emp/listAllEmp2_getFromSession.jsp";
+			RequestDispatcher successView = req.getRequestDispatcher(url);// ���\���listAllEmp2_getFromSession.jsp
+			successView.forward(req, res);
+			return;
+		}
+
+
+		if ("getOne_For_Display".equals(action)) { // �Ӧ�select_page.jsp���ШD
+
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+
+			try {
+				/***************************1.�����ШD�Ѽ� - ��J�榡�����~�B�z**********************/
+				String str = req.getParameter("empno");
+				if (str == null || (str.trim()).length() == 0) {
+					errorMsgs.add("�п�J���u�s��");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/emp/select_page.jsp");
+					failureView.forward(req, res);
+					return;//�{�����_
+				}
+				
+				Integer empno = null;
+				try {
+					empno = new Integer(str);
+				} catch (Exception e) {
+					errorMsgs.add("���u�s���榡�����T");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/emp/select_page.jsp");
+					failureView.forward(req, res);
+					return;//�{�����_
+				}
+				
+				/***************************2.�}�l�d�߸��*****************************************/
+				EmpDAO dao = new EmpDAO();
+				EmpVO empVO = dao.findByPrimaryKey(empno);
+				if (empVO == null) {
+					errorMsgs.add("�d�L���");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/emp/select_page.jsp");
+					failureView.forward(req, res);
+					return;//�{�����_
+				}
+				
+				/***************************3.�d�ߧ���,�ǳ����(Send the Success view)*************/
+				req.setAttribute("empVO", empVO); // ��Ʈw���X��empVO����,�s�Jreq
+				String url = "/emp/listOneEmp.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // ���\��� listOneEmp.jsp
+				successView.forward(req, res);
+
+				/***************************��L�i�઺���~�B�z*************************************/
+			} catch (Exception e) {
+				errorMsgs.add("�L�k���o���:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/emp/select_page.jsp");
+				failureView.forward(req, res);
+			}
+		}
+	}
+}
